59e6377c66013bb1c5e51abe06e263893b998683
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-03-29 23:46:54 +0100

    start over

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { Canvas } from "@react-three/fiber";
-import { Cylinder, PerspectiveCamera } from "@react-three/drei";
+import {
+  Cylinder,
+  PerspectiveCamera,
+  KeyboardControls,
+} from "@react-three/drei";
 import "./App.css";
 import { Plane } from "./Plane";
-import { Player } from "./Player";
-import { Physics } from "use-cannon";
 
 function App() {
   return (
     <>
+      <KeyboardControls />
       <Canvas>
         <PerspectiveCamera makeDefault position={[3, 3, 3]} />
-        <Physics gravity={[0, -30, 0]}>
-          <Plane />
-          <Player />
-        </Physics>
+
+        <Plane />
+
         <Cylinder />
         <hemisphereLight intensity={0.5} />
       </Canvas>
--- src/Plane.jsx
+++ src/Plane.jsx
@@ -3,26 +3,19 @@ import * as THREE from "three";
 
 export function Plane() {
   var g2 = new THREE.PlaneGeometry(2000, 2000, 8, 8);
-  var m2 = new THREE.MeshPhongMaterial(
-//     {
-//     color: "#2c61d4",
-//   }
-  );
+  var m2 = new THREE.MeshPhongMaterial();
 
-  
-  const texture = new THREE.TextureLoader().load( "/tex.jpg" );
+  const texture = new THREE.TextureLoader().load("/tex.jpg");
   texture.wrapS = THREE.RepeatWrapping;
   texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set( 200, 200 );
-  
+  texture.repeat.set(200, 200);
+
   m2.map = texture;
 
   var plane = new THREE.Mesh(g2, m2);
   plane.rotateX(-Math.PI / 2);
   plane.translateY(1);
   plane.receiveShadow = true;
-
-  
 
   return <primitive object={plane} />;
 }
